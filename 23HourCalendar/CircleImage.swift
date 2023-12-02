af46311a0e16b71918649fd53db4a1e5635b4333
Will Dirkswager <Will@dirkback.com>
2023-12-02 12:16:09 -0600

    Get Started

---
+++ 23HourCalendar/CircleImage.swift
@@ -0,0 +1,18 @@
+//
+//  CircleImage.swift
+//  23HourCalendar
+//
+//  Created by Will Dirkswager on 12/2/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
---
+++ 23HourCalendar/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  23HourCalendar
+//
+//  Created by Will Dirkswager on 12/2/23.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
