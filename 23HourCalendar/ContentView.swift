116f64eadfd509e8d073c87fc343ad3cc59a3936
Will Dirkswager <Will@dirkback.com>
2023-12-02 11:44:16 -0600

    Initial Commit

---
+++ 23HourCalendar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  23HourCalendar
+//
+//  Created by Will Dirkswager on 12/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 23HourCalendar/_3HourCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  _3HourCalendarApp.swift
+//  23HourCalendar
+//
+//  Created by Will Dirkswager on 12/2/23.
+//
+
+import SwiftUI
+
+@main
+struct _3HourCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
